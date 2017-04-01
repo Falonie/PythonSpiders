8c51ce1afba8ba4c132631ae8f0ba4001c46f102
Falonie <541002901@qq.com>
2017-04-01 20:06:12 +0800

    lagou

---
+++ Crawl lagou Data Analyst company&salary/Crawl lagou Data Analyst company&salary_2.py
@@ -0,0 +1,39 @@
+import requests,re,time,csv
+from bs4 import BeautifulSoup
+
+class Lagou(object):
+    def lagou(some_url):
+        try:
+            cookie = {
+                'Cookie': 'user_trace_token=20170326001734-ec41389ced6b4a4882609bf9e467bdb2; LGUID=20170326001734-8e3315f4-1176-11e7-9569-5254005c3644; TG-TRACK-CODE=deliver_guess; JSESSIONID=197AA9B8333FB8CB7DF60C237916266D; _putrc=43700CED3E324D1B; login=true; unick=%E7%8E%8B%E6%99%B4%E6%99%A8; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=5; _gat=1; LGSID=20170329210923-ee02d0e2-1480-11e7-a717-525400f775ce; LGRID=20170329214841-6b11e72a-1486-11e7-9574-5254005c3644; SEARCH_ID=81650aaa6c154f45a2b11a1d4107a646; index_location_city=%E4%B8%8A%E6%B5%B7; _ga=GA1.2.1946463376.1490458642; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1490458642; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1490795326'}
+            header = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+            session = requests.session()
+
+            for i in range(1,31):
+                url = some_url.format(i)
+            #    r = requests.get(url, cookies=cookie, headers=header).text
+                r = session.get(url, cookies=cookie, headers=header).content
+                bsobj = BeautifulSoup(r, 'html.parser')
+                pattern = re.compile(r'<span class="money">(.*?)</span>')
+                pattern2 = re.compile(r'class="con_list_item default_list" data-company=(.*?) data-companyid=')
+                pattern3 = re.compile(r'<span class="add">\[<em>(.*?)</em>\]')
+                company = pattern.findall(str(bsobj))
+                salary = pattern2.findall(str(bsobj))
+                block = pattern3.findall(str(bsobj))
+                print('Page %s'%i)
+                for c,s,b in zip(company,salary,block):
+                    #with open('lagou.csv', 'a+', encoding='utf-8') as f:
+                    with open('lagou2.txt', 'a+', encoding='utf-8') as f:
+                        f.write(c);f.write(s);f.write(b + '\n')
+                    print(c,s,b)
+                time.sleep(.5)
+                print('\n')
+        except Exception as e:
+            pass
+        else:
+            print('Success to crawl lagou company and salary!')
+
+base_url='https://www.lagou.com/zhaopin/shujufenxishi/{}/?filterOption=3'
+a=Lagou
+a.lagou(base_url)
---
+++ crawl ipanda comments/craw ipanda comments.py
@@ -0,0 +1,22 @@
+import requests,json,re,time,os,csv
+
+base_url='http://weibo.com/aj/v6/comment/big?ajwvr=6&id=4090258591184334&root_comment_max_id=13883125907851111&root_comment_max_id_type=0&root_comment_ext_param=&page={page}&filter=hot'
+
+cookie={'Cookie':'TC-Ugrow-G0=0149286e34b004ccf8a0b99657f15013; login_sid_t=bfc63a0a4ed30c3c3f14fe6608f2693f; TC-V5-G0=c427b4f7dad4c026ba2b0431d93d839e; _s_tentry=-; Apache=614236817117.7325.1490449015297; SINAGLOBAL=614236817117.7325.1490449015297; ULV=1490449015314:1:1:1:614236817117.7325.1490449015297:; SSOLoginState=1490449107; TC-Page-G0=b1761408ab251c6e55d3a11f8415fc72; wb_publish_fist100_2682846513=1; UOR=,,www.google.com.hk; SCF=AmovXSi_nKdmLFHZHOKMOmVZmKgHLCAdrPaLd4KDRHJfYMgxi0skavY2XGc-l3mgV9dF_6W0OxdwyUI2PgNqPyg.; SUB=_2A2513yYBDeRxGeRI41AZ9CjJyj-IHXVWrRDJrDV8PUNbmtAKLWjYkW8AF7_G0CqDwO0KxufgM5G2Kby5Uw..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9Wh-HT3hyw4nEHp3KKm4KI.m5JpX5KzhUgL.Fozc1hzRShqfeKe2dJLoI79hqPSLdr.t; SUHB=0ftYKbX9Ce1ICB; ALF=1522305488; wvr=6'}
+header={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+pattern=re.compile(r'</a>(.*?)</div>')
+chinese_pattern=re.compile(r'[\u4e00-\u9fa5]+')
+
+for i in range(1,6,1):
+    url = base_url.format(page=i)
+    r=requests.get(url,cookies=cookie,headers=header).json()
+    data=r['data']['html']
+    comment=pattern.findall(data)
+    for i in comment:
+        chinese_comment=chinese_pattern.findall(i)
+        chinese_comment=''.join(chinese_comment)+'\n'
+        with open('ipanda_live.csv','a+',encoding='utf-8') as f:
+            writer=csv.writer(f)
+            writer.writerow(chinese_comment)
+        print(chinese_comment)
+    time.sleep(.5)
