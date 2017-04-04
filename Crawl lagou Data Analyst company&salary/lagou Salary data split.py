546b6277d0d194a8c635504cc69d6bcd06bd637a
Falonie <541002901@qq.com>
2017-04-04 17:05:55 +0800

    lagou Salary Data Visualization

--- Crawl lagou Data Analyst company&salary/lagou Salary data split.py
+++ Crawl lagou Data Analyst company&salary/lagou Salary data split.py
@@ -3,7 +3,7 @@ import re,csv
 
 data=pd.read_csv('lagou_update.csv')
 #This file is intended to split the column 'Salary' into 'Salary1' and 'Salary2'.
-#拆分Salary列为工资相对较低的列'Salary'和相对较高的列'Salary2'
+#拆分Salary列为工资相对较低的列'Salary1'和相对较高的列'Salary2'
 
 with open('Salary1.txt', 'w+',encoding='utf-8') as f,open('Salary2.txt', 'w+',encoding='utf-8') as f2:
     for line in data['Salary']:
