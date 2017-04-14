f86dcde5f211a865cc2619a08900908207593237
Falonie <541002901@qq.com>
2017-04-14 21:49:25 +0800

    lagou Salary Data Visualization

--- Crawl lagou Data Analyst company&salary/Data Analyst's Salary data Analysis report.py
+++ Crawl lagou Data Analyst company&salary/Data Analyst's Salary data Analysis report.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 
 data=pd.read_csv('lagou_update.csv')
-print(data.describe(),'\n')
-
+#print(data.describe(),'\n')
 #print(data['Salary1'].value_counts(),'\n')
+
+data2=pd.read_csv('lagou_hangzhou.csv')
+print(data2['Experience'].value_counts())
