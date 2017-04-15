0fea7bdf29c869ab224d399e6cc51a7f1b3f74b7
Falonie <541002901@qq.com>
2017-04-15 22:06:52 +0800

    lagou Data Analyst's Salary data analysis report

--- Crawl lagou Data Analyst company&salary/Crawl lagou Data Analyst details in Hangzhou.py
+++ Crawl lagou Data Analyst company&salary/Crawl lagou Data Analyst details in Hangzhou.py
@@ -1,39 +1,21 @@
-import requests,re,time,csv
+import requests,re
 from bs4 import BeautifulSoup
+from lxml import html
 
-class Lagou(object):
-    def lagou(some_url):
-        try:
-            cookie = {
-                'Cookie': 'user_trace_token=20170326001734-ec41389ced6b4a4882609bf9e467bdb2; LGUID=20170326001734-8e3315f4-1176-11e7-9569-5254005c3644; TG-TRACK-CODE=deliver_guess; JSESSIONID=197AA9B8333FB8CB7DF60C237916266D; _putrc=43700CED3E324D1B; login=true; unick=%E7%8E%8B%E6%99%B4%E6%99%A8; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=5; _gat=1; LGSID=20170329210923-ee02d0e2-1480-11e7-a717-525400f775ce; LGRID=20170329214841-6b11e72a-1486-11e7-9574-5254005c3644; SEARCH_ID=81650aaa6c154f45a2b11a1d4107a646; index_location_city=%E4%B8%8A%E6%B5%B7; _ga=GA1.2.1946463376.1490458642; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1490458642; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1490795326'}
-            header = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
-            session = requests.session()
+url = 'https://www.lagou.com/zhaopin/shujufenxishi/3/?filterOption=2'
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+cookie = {
+    'Cookie': 'user_trace_token=20170326001734-ec41389ced6b4a4882609bf9e467bdb2; LGUID=20170326001734-8e3315f4-1176-11e7-9569-5254005c3644; JSESSIONID=197AA9B8333FB8CB7DF60C237916266D; _putrc=43700CED3E324D1B; login=true; unick=%E7%8E%8B%E6%99%B4%E6%99%A8; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=8; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2F; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; TG-TRACK-CODE=index_navigation; SEARCH_ID=47ef26b4046945ae8677be130dd18a7a; index_location_city=%E6%9D%AD%E5%B7%9E; _gat=1; _ga=GA1.2.1946463376.1490458642; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1490458642,1491055889; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1492262327; LGSID=20170415210122-9fdb72b0-21db-11e7-a32c-5254005c3644; LGRID=20170415211900-1697c1a5-21de-11e7-b5fa-525400f775ce'}
 
-            for i in range(1,31):
-                url = some_url.format(i)
-                #    r = requests.get(url, cookies=cookie, headers=header).text
-                r = session.get(url, cookies=cookie, headers=header).content
-                bsobj = BeautifulSoup(r, 'html.parser')
-                pattern = re.compile(r'<span class="money">(.*?)</span>')
-                pattern2 = re.compile(r'class="con_list_item default_list" data-company=(.*?) data-companyid=')
-                pattern3 = re.compile(r'<span class="add">\[<em>(.*?)</em>\]')
-                pattern4 = re.compile(r'<!--<i></i>-->(.*?)/')
-                pattern5 = re.compile(r'<div class="industry">(\s*)(.*?)/')
-                company = pattern.findall(str(bsobj))
-                salary = pattern2.findall(str(bsobj))
-                block = pattern3.findall(str(bsobj))
-                experience = pattern4.findall(str(bsobj))[:15]
-                category = pattern5.findall(str(bsobj))
-                print('Page %s' % i)
-                for c, s, b, e, r in zip(company, salary, block, experience, category[:15]):
-                    print(c, s, b, e, r[1])
-                print('\n')
-        except Exception as e:
-            pass
-        else:
-            print('Success to crawl lagou company and salary!')
-
-base_url='https://www.lagou.com/zhaopin/shujufenxishi/{}/?filterOption=3'
-a=Lagou
-a.lagou(base_url)
+for i in range(1,31):
+    session = requests.session()
+    response = session.get(url, headers=header, cookies=cookie).text
+    bsobj = BeautifulSoup(response, 'html.parser')
+    sel = html.fromstring(response)
+    company_name = sel.xpath('//div[@class="company_name"]/a/text()')
+    block = sel.xpath('//div[@class="p_top"]/a/span/em/text()')
+    salary = sel.xpath('//span[@class="money"]/text()')
+    experience = re.findall(r'<!--<i></i>-->(.*?)/', str(bsobj))
+    for c, s, e, b in zip(company_name, salary, experience, block):
+        print(c, s, e, b)
