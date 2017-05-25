b6effd6c93acc939452d3e28bec1e37c69857079
Wangqingchen <541002901@qq.com>
2017-05-25 17:49:34 +0800

    Logging Cookbook

---
+++ Python Standard Libary Learning/Python HOWTOS/Logging Cookbook/auxiliary_module.py
@@ -0,0 +1,17 @@
+import logging
+
+module_logger=logging.getLogger('spam_application.zuxiliary')
+
+class Auxiliary():
+
+    def __init__(self):
+        self.logger = logging.getLogger('spam_application.auxiliary.Auxiliary')
+        self.logger.info('creating an instance of Auxiliary')
+
+    def do_something(self):
+        self.logger.info('doing someing')
+        a = 1 + 1
+        self.logger.info('done doing something')
+
+def some_function():
+    module_logger.info('received a call to "some_function"')
