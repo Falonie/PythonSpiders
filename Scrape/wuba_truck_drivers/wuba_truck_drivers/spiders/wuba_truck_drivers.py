a917da492ac51f8bd289ba50504c611a4ec72684
Wangqingchen <541002901@qq.com>
2017-06-12 15:30:49 +0800

    truck_drivers

--- Scrape/wuba_truck_drivers/wuba_truck_drivers/spiders/wuba_truck_drivers.py
+++ Scrape/wuba_truck_drivers/wuba_truck_drivers/spiders/wuba_truck_drivers.py
@@ -3,8 +3,18 @@ import scrapy
 class WubaTruckDrivers(scrapy.Spider):
 
     name = 'wuba_truck_drivers'
-    start_urls=['http://sh.58.com/sonhuosiji/?PGTID=0d303668-0000-2729-3bbd-759ea9bb0785&ClickID=2']
+    start_urls=['http://sh.58.com/sonhuosiji/pn{page}/?PGTID=0d302517-0000-2758-37fc-c2a3b0f3231b&ClickID=4']
+
+    def start_requests(self):
+        for i in range(1, 9):
+            url = self.start_urls[0].format(page=i)
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for href in response.xpath('//div[@id="infolist"]/dl/dt/a/text()').extract():
-            yield {'href':href}
+        for item in response.xpath('//div[@id="infolist"]/dl/dt'):
+            # yield {'title':item.xpath('a/text()').extract_first(),
+            #        'href':item.xpath('a/@href').extract_first()}
+            yield scrapy.Request(url=item.xpath('a/text()').extract_first(), callback=self.driver_details)
+
+    def driver_details(self,response):
+        yield {''}
